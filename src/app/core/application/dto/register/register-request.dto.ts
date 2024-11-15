f5c365e231f49093042a42a6cdbfcb2d88a4e4c8
ecc97 <carmonaedwin1123@gmail.com>
2024-11-15 07:27:35 -0500

    fix: there is not register, only login

--- src/app/core/application/dto/register/register-request.dto.ts
+++
@@ -1,7 +0,0 @@
-interface IRegisterResquest {
-    email: string;
-    password: string;
-    name: string;
-    role: string;
-    photo?: File | null;
-}
--- src/components/ui/templates/index.ts
+++ src/components/ui/templates/index.ts
@@ -1,3 +1,1 @@
 export * from './auth/LoginTemplate';
-export * from './auth/RegisterTemplate';
-export * from './homePage/HomeTemplate';
